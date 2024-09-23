040b041bf3c18e40ad71acce82d7f52b02a1e054
SoyGLTech <soygltech@gmail.com>
2024-09-23 13:53:55 +0200

    Agregamos los vinculos de indes.js, App.js y app.scss

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss"
+
+function App () {
+    return (
+        <div className="app">
+            <h1>React + Webpack +SASS</h1>
+            <p>
+                Mi primer projecto con React
+            </p>
+        </div>
+    )
+}
